185b96021dfb8fdd6d48cf5b8797acc3136b4eff
ClementPan <zzz2999582@gmail.com>
2021-04-29 16:38:10 +0800

    fix: oranged userSidebar

--- src/views/PublicMessage.vue
+++ src/views/PublicMessage.vue
@@ -6,10 +6,34 @@
     <!-- mainSection -->
     <div class="mainSection">
       <!-- onlineUserList -->
-      <h1>上線使用者</h1>
+      <div class="usersOnline">
+        <div class="title">
+          <h1>上線使用者</h1>
+        </div>
+        <div class="container">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. In
+            reprehenderit reiciendis dolore illo ipsa vitae excepturi cumque
+            incidunt eum voluptate obcaecati itaque quod architecto, officiis
+            ipsum repudiandae dignissimos. Assumenda, officia.
+          </p>
+        </div>
+      </div>
 
       <!-- messageBox -->
-      <h1>公開聊天室</h1>
+      <div class="messageBox">
+        <div class="title">
+          <h1>公開聊天室</h1>
+        </div>
+        <div class="container">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. In
+            reprehenderit reiciendis dolore illo ipsa vitae excepturi cumque
+            incidunt eum voluptate obcaecati itaque quod architecto, officiis
+            ipsum repudiandae dignissimos. Assumenda, officia.
+          </p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -26,12 +50,45 @@ export default {
 <style scoped>
 .publicMessage {
   display: flex;
+  /* border: 1px solid #000; */
 }
 
 .mainSection {
   flex: 1;
   width: 100%;
   height: 100vh;
-  border: 1px solid #000;
+  border-right: 1px solid #e6ecf0;
+  /* border: 3px solid red; */
+  display: flex;
+}
+
+.usersOnline {
+  border-right: 1px solid #e6ecf0;
+  flex: 1;
+}
+
+.messageBox {
+  /* border: 1px solid #000; */
+  flex: 2;
+}
+
+.usersOnline .title,
+.messageBox .title {
+  border-bottom: 1px solid #e6ecf0;
+  height: 55px;
+  padding: 10px 15px;
+  display: flex;
+  align-items: center;
+  position: sticky;
+  top: 0;
+  background-color: #fff;
+  z-index: 1;
+  /* z-index: 999; */
+}
+
+.title h1 {
+  font-weight: 700;
+  font-size: 19px;
+  margin: 0;
 }
 </style>
