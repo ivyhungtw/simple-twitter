70f69bab16e63f8c19cc6fd7aca2c4f04270c988
ClementPan <zzz2999582@gmail.com>
2021-04-29 10:17:13 +0800

    fix: profilecard avatar

--- src/components/UserProfileCard.vue
+++ src/components/UserProfileCard.vue
@@ -290,6 +290,13 @@ export default {
   z-index: 999;
   overflow: hidden;
   object-fit: cover;
+  border: 1px solid #000;
+}
+
+.avatar img {
+  width: 100%;
+  height: 100%;
+  /* border: 1px solid #000; */
 }
 
 .avatar:after {
